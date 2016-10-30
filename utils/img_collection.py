c913f90e2d467f297479dd899b87dfad65e885c6
Ivan Khomyakov <khaxis@Ivans-MacBook-Pro.local>
2016-10-30 23:38:53 +0300

    add functions

--- utils/img_collection.py
+++ utils/img_collection.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from db_connector import *
+from bson.objectid import ObjectId
 
 def getDesctiption(nId):
     res = db.wordnet_words.find_one({'NId':nId})
@@ -8,16 +10,23 @@ def getDesctiption(nId):
     return None
 
 
-def findChildren(sourceNode, maxDepth=100):
+def findChildren(sourceNode, maxDepth=100, excludeNIds = None):
     node = {'NId':sourceNode, 'children': list(), 'depth':0, 'description':getDesctiption(sourceNode)}
     res = node
     nodesToVisit = [node]
+    if excludeNIds is not None:
+        excludeNIds = set(excludeNIds)
+    else:
+        excludeNIds = set()
+
     while len(nodesToVisit) > 0:
         node = nodesToVisit[0]
         nodesToVisit = nodesToVisit[1:]
         if node['depth'] >= maxDepth:
             continue
         for row in db.wordnet_is_a.find({'NId':node['NId']}):
+            if row['subNId'] in excludeNIds:
+                continue
             newNode = {'NId':row['subNId'], 'children': list(), 'depth':node['depth']+1, 'description':getDesctiption(row['subNId'])}
             node['children'].append(newNode)
             nodesToVisit.append(newNode)
@@ -29,11 +38,11 @@ def findParents(sourceNode):
     node = {'NId':sourceNode, 'description':getDesctiption(sourceNode)}
     nodesToVisit = [node]
     while True:
-        res.append(node)
         parent = db.wordnet_is_a.find_one({'subNId':node['NId']})
         if parent is None:
             break
         node = {'NId':parent['NId'], 'description':getDesctiption(parent['NId'])}
+        res.append(node)
     return res
 
 
@@ -58,7 +67,41 @@ def getTreeUrls(tree):
     
     return res
 
+def getFlattenUrlsTree(tree):
+    res = []
+    nIds = []
+    getNIdsRecursive(tree, nIds)
+    for row in db.image_urls.find({'$or': [{'NId':nid} for nid in nIds]}):
+        res.append(row)
+    return res
 
-def findRandomDocuments(collection, randomDocumentsCount):
+
+def _findRandomDocuments(collection, randomDocumentsCount):
     randomDocumentsRatio = float(randomDocumentsCount) / collection.count()
     return collection.find({'$where':"function () {return Math.random()<%f}" % randomDocumentsRatio})
+
+
+def findRandomImageUrlsDocuments(randomDocumentsCount):
+    res = []
+    for row in _findRandomDocuments(db.image_urls, randomDocumentsCount):
+        res.append(row)
+    return res
+
+
+def makePool(description):
+    return db.pool.insert({'description':description, 'date_inserted':datetime.datetime.utcnow()})
+
+
+def assignToPool(urlRecord, poolId, target):
+    db.image_urls.update_one(
+        {'_id':urlRecord['_id']},
+        {'$push':
+            {
+            'pools':
+                {
+                'poolId': poolId,
+                'target':target
+                }
+            }
+        }
+    )
