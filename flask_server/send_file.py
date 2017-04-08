fb487899c01f63fce07f769158035e431391fabf
Ivan Khomyakov <khaxis@gmail.com>
2017-04-08 20:17:55 +0300

    using boto instead of local files

--- flask_server/send_file.py
+++ flask_server/send_file.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
-from flask_server.common import *
+from flask import send_file
 from flask_server.pool import *
 from os.path import join
 from utils.config import getDataPath
+import io
+
+from boto import storage_uri
+from gcs_oauth2_boto_plugin import oauth2_plugin
 
 
 @app.route("/get_image/<image_path>")
 def get_image(image_path):
-    return send_from_directory(join(getDataPath(), 'images'), image_path)
+    root = 'gs://image_tags/images/'
+    content = storage_uri(root + image_path)
+    image_file = io.BytesIO()
+    content.get_contents_to_file(image_file)
+    image_file.seek(0)
+    return send_file(image_file, attachment_filename=image_path, as_attachment=False)
--- utils/db_connector.py
+++ utils/db_connector.py
@@ -4,7 +4,8 @@ from utils.config import GetDBConfig
 connectionConfig = GetDBConfig()
 client = MongoClient(connectionConfig.host, connectionConfig.port)
 db = client['image_tags']
-db.authenticate(connectionConfig.user, connectionConfig.password)
+if connectionConfig.user:
+    db.authenticate(connectionConfig.user, connectionConfig.password)
 
 if __name__ == '__main__':
     for p in db.pool.find():
