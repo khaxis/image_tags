0fbe1511695b4407601f94881338999a276a3013
Ivan Khomyakov <khaxis@gmail.com>
2017-04-16 10:42:14 +0300

    handle zero sized files

--- utils/image_handler.py
+++ utils/image_handler.py
@@ -4,5 +4,7 @@ from utils.file_handler import get_stream
 
 def get_image(image_path, cv2_img_flag=0):
     """Get OpenCV like image"""
-    img_array = np.asarray(bytearray(get_stream(image_path)), dtype=np.uint8)
-    return cv2.imdecode(img_array, cv2_img_flag)
+    img_array = np.asarray(bytearray(get_stream(image_path).read()), dtype=np.uint8)
+    if len(img_array):
+        return cv2.imdecode(img_array, cv2_img_flag)
+    return None
