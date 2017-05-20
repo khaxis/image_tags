4386d6809d4ea484c47c3366a909dea1f947d92c
Ivan Khomyakov <khaxis@gmail.com>
2017-05-20 16:45:32 +0300

    use concurent jobs to download the images

--- learning_utils/fetch_pool.py
+++ learning_utils/fetch_pool.py
@@ -10,13 +10,42 @@ from utils import file_handler
 from utils import image_handler
 from features import sliceFactory
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def parseArguments():
     parser = argparse.ArgumentParser(description='Fetch all urls from the pool')
     parser.add_argument('--pool', dest='pool', help='Pool id', required=True)
     parser.add_argument('--test-mode', help='Do nothing, but return prepared pool', action='store_true')
+    parser.add_argument('--threads-number', help='The number of concurent threads', type=int, default=10)
 
     return parser.parse_args()
+
+
+def downloadAndUpdateImage(storePath, row):
+    if 'path' not in row:
+        destination = os.path.join(storePath, row['IId'])
+        file_content = web_utils.get_file_stream(row['Url'])
+        if file_content and image_handler.is_valid_image(file_content):
+            try:
+                file_handler.upload_file_stream(destination, file_content)
+            except:
+                pass
+            # the image successfully saved
+            icoll.updateImagePath(row, destination)
+            icoll.updateImageDownloadableStatus(row, True)
+            icoll.updateImageValidStatus(row, True)
+        else:
+            icoll.updateImageDownloadableStatus(row, False)
+            icoll.updateImageValidStatus(row, False)
+    else:
+        # temporary
+        file_path = row['path']
+        if file_path.startswith('/Users'):
+            file_path = 'image_tags/images/' + row['IId']
+            icoll.updateImagePath(row, file_path)
+        file_content = file_handler.get_file_stream(file_path)
+        valid_status = image_handler.is_valid_image(file_content)
+        icoll.updateImageValidStatus(row, valid_status)
 
 
 def fetchPool(argv):
@@ -31,37 +60,21 @@ def fetchPool(argv):
 
     i = 0
     totalCount = pcoll.getPoolSize(args.pool)
-    with icoll.getPoolUrlsIterator(args.pool) as cursor:
-        cursor.batch_size(100)
-        for row in cursor:
-            destination = os.path.join(storePath, row['IId'])
-            if 'path' not in row:
-                file_content = web_utils.get_file_stream(row['Url'])
-                if file_content and image_handler.is_valid_image(file_content):
-                    try:
-                        file_handler.upload_file_stream(destination, file_content)
-                    except:
-                        pass
-                    # the image successfully saved
-                    icoll.updateImagePath(row, destination)
-                    icoll.updateImageDownloadableStatus(row, True)
-                    icoll.updateImageValidStatus(row, True)
-                else:
-                    icoll.updateImageDownloadableStatus(row, False)
-                    icoll.updateImageValidStatus(row, False)
-            else:
-                # temporary
-                file_path = row['path']
-                if file_path.startswith('/Users'):
-                    file_path = 'image_tags/images/' + row['IId']
-                    icoll.updateImagePath(row, file_path)
-                file_content = file_handler.get_file_stream(file_path)
-                valid_status = image_handler.is_valid_image(file_content)
-                icoll.updateImageValidStatus(row, valid_status)
+    futures = []
+    with ThreadPoolExecutor(max_workers=args.threads_number) as executor:
+        with icoll.getPoolUrlsIterator(args.pool) as cursor:
+            cursor.batch_size(100)
+            for row in cursor:
+                future = executor.submit(downloadAndUpdateImage, storePath, row)
+                futures.append(future)
+            print()
 
+        print("Executors scheduled")
+        for f in as_completed(futures):
             i += 1
             progress_bar.printProgress(i, totalCount)
         print()
+
 
     print("Pool cached")
     print("Extracting features...")
